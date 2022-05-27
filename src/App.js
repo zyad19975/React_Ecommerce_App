c95e0a709ae745d59e784c1990444205102218b8
zyad mohamed <zyad19975@gmail.com>
2022-05-27 13:22:09 +0200

    creating the main page with products and navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { commerce } from "./lib/commerce";
+
+import { Products, Navbar } from "./components";
 
 const App = () => {
-  return <div>E-commerce</div>;
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <Products products={products} />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  MenuItem,
+  Menu,
+  Typography,
+} from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
+import { mergeClasses } from "@material-ui/core/node_modules/@material-ui/styles";
+import useStyles from "./style";
+
+import logo from "../../assets/commerce.png";
+
+const Navbar = () => {
+  const classes = useStyles();
+  return (
+    <div>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <Typography varient="h6" className={classes.title} color="inherit">
+            <img
+              src={logo}
+              alt="Commerce.js"
+              height="25px"
+              className={classes.image}
+            />
+            E-Commerce
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.button}>
+            <IconButton arial-lable="Show Cart Items" color="ingerit">
+              <Badge badgeContent={3} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default Navbar;
